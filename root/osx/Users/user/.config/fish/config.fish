819eb6b5ba9dbd7286e0447ea4135112d16a730d
Jianjun Mao <justmao945@gmail.com>
2014-03-22 12:43:46 +0800

    add aria2d

--- root/osx/Users/user/.config/fish/config.fish
+++ root/osx/Users/user/.config/fish/config.fish
@@ -2,22 +2,25 @@
 #    alias      |
 #---------------+
 function c  --description 'Clear screen'
-    clear
+  clear
 end
 
 function df --description 'Print sizes in human readable'
-    command df -h $argv
+  command df -h $argv
 end
 
 function man --description 'Use vim viewer to display manpage'
-    command man $argv | col -b | command view -c 'set ft=man ts=8 nomod nolist' -
+  command man $argv | col -b | command view -c 'set ft=man ts=8 nomod nolist' -
 end
 
 function rm --description 'Remove interactively'
-    command rm -i $argv
+  command rm -i $argv
 end
 
 function mkproj_cocos2dx3 --description 'create_project.py for Cocos2d-x 3.0'
-    python $HOME/github/cocos2d-x/tools/project-creator/create_project.py $argv
+  python $HOME/github/cocos2d-x/tools/project-creator/create_project.py $argv
 end
 
+function start_aria2d --description 'Start Aria2 RPC daemon'
+  aria2c --enable-rpc --rpc-listen-all $argv
+end
