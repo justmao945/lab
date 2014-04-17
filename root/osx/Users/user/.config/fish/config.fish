189d6859074cad5cef4ba97da55db4bae027f6f9
jianjun <jianjun@atrenta.com>
2014-04-17 23:11:41 +0800

    update

--- root/osx/Users/user/.config/fish/config.fish
+++ root/osx/Users/user/.config/fish/config.fish
@@ -32,12 +32,3 @@ end
 function mkproj_cocos2dx3 --description 'create_project.py for Cocos2d-x 3.0'
   python $HOME/github/cocos2d-x/tools/project-creator/create_project.py $argv
 end
-
-function start_aria2d --description 'Start Aria2 RPC daemon'
-  aria2c --enable-rpc --rpc-listen-all $argv
-end
-
-function start_goagent --description 'Start GoAgent Proxy daemon'
-  python $HOME/github/3rd/goagent/local/proxy.py $argv
-end
-
