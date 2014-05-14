d79b404487d27e57a7e2c3855fffe28fa724dbb0
jianjun <justmao945@gmail.com>
2014-05-14 22:15:58 +0800

    add gameplay

---
+++ objc/PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Jianjun on 14/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
