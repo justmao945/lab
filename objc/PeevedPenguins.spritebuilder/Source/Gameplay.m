21e144b519773a52841cda5a171861e437ab91b6
jianjun <justmao945@gmail.com>
2014-05-15 22:56:44 +0800

    add level

--- objc/PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ objc/PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,15 +12,30 @@
 {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
+    CCNode *_levelNode;
+    CCNode *_gameNode;
+}
+
+-(void)retry
+{
+    [[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"Gameplay"]];
 }
 
 -(void)didLoadFromCCB
 {
     // tell this scene to accept touches
     self.userInteractionEnabled = YES;
+    
+    CCScene* level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    [self launchPenguins];
+}
+
+-(void)launchPenguins
 {
     CCNode *penguin = [CCBReader load:@"Penguin"];
     penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
@@ -30,7 +45,16 @@
     CGPoint launchDirection = ccp(1, 0);
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
+    
+    // ensure followed object is in visible are when starting
+    // reset position to left most when launch
+    _gameNode.position = ccp(0,0);
+    CCLOG(@"%f %f", self.position.x, self.position.y);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:_gameNode.boundingBox];
+    [_gameNode runAction:follow];
 }
+
+
 
 
 
