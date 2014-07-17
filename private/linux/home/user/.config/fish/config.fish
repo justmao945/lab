50a252756824899c060b6876d9974eb58d56cd95
jianjun <justmao945@gmail.com>
2014-07-17 10:14:28 +0800

    update

--- private/linux/home/user/.config/fish/config.fish
+++ private/linux/home/user/.config/fish/config.fish
@@ -1,50 +1,50 @@
 #---------------+
 #    alias      |
 #---------------+
-function c  --description 'Clear screen'
+function c  -d 'Clear screen'
     clear
 end
 
-function cmake-clang --description 'Use clang as the C/C++ compiler'
+function cmake-clang -d 'Use clang as the C/C++ compiler'
     command cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang $argv
 end
 
-function df --description 'Print sizes in human readable'
+function df -d 'Print sizes in human readable'
     command df -h $argv
 end
 
-function man --description 'Use vim viewer to display manpage'
+function man -d 'Use vim viewer to display manpage'
     command man $argv | col -b | command view -c 'set ft=man ts=8 nomod nolist' -
 end
 
-function sudo --description 'Use user env to call sudo'
+function sudo -d 'Use user env to call sudo'
     command sudo -E $argv
 end
 
-function rm --description 'Remove interactively'
+function rm -d 'Remove interactively'
     command rm -i $argv
 end
 
 # htop can't refresh when TERM is rxvt-unicode-256color
-function t  --description 'Resource top usage'
+function t  -d 'Resource top usage'
     env TERM=screen htop $argv
 end
 
-function tsocks --description 'User config tsocks'
-    env TSOCKS_CONF_FILE=$HOME/.tsocks.conf tsocks $argv
-end
-
-function ssh --description 'SSH'
+function ssh -d 'SSH'
     env TERM=screen ssh $argv
 end
 
-function start_tunnel --description 'Start SSH tunnel on localhost on port :1314'
+function start_tunnel -d 'Start SSH tunnel on localhost on port :1314'
   while [ true ]
     echo 'Connect and listen on port 1314...'
     ssh -vvvCTD 1314 linode
     echo 'Retry in 3 seconds...'
     sleep 3
   end
+end
+
+function tmux -d 'Start with TERM=screen-256color-bce'
+    env TERM=screen-256color-bce tmux $argv
 end
 
 function with_proxy -d 'Start under HTTP proxy localhost:1315'
@@ -67,7 +67,7 @@ set -x XIM              fcitx
 #    tmux title |
 #---------------+
 # Set the tmux window title, depending on whether we are running something, or just prompting
-function fish_title --description 'Set tmux title'
+function fish_title -d 'Set tmux title'
     set max_length 19
     if [ "fish" != $_ ] # command as title
         tmux rename-window "$_ $argv"
@@ -88,4 +88,3 @@ set config_ohio $HOME/.config/fish/config_ohio.fish
 if [ -f $config_ohio ]
     source $config_ohio
 end
-
