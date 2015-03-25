2e21cc74a6cbac059249488e718334c73b069e7e
jianjun <justmao945@gmail.com>
2015-03-25 14:33:11 +0800

    update

--- smtp.rb/send.rb
+++ smtp.rb/send.rb
@@ -2,19 +2,22 @@ require 'net/smtp'
 
 from = "test@test.com"
 to = "justmao945@gmail.com"
+cc = "327118385@qq.com"
+
+to, cc = cc, to
 
 msg = <<EOF
-
 From: demo <#{from}>
 To: Jianjun Mao <#{to}>
+Cc: Jianjun Mao <#{cc}>
 Subject: SMTP.rb test
 Date: #{Time.now}
  
-HaHa, this mail is sent by ruby.
 
+HaHa, this mail is sent by ruby.
 EOF
 
 Net::SMTP.start('localhost') do |smtp|
-    smtp.send_message msg, from, to
+    smtp.send_message msg, from, [to,cc]
 end
 
