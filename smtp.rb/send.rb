0fa20b049e1370f8604022fd3249319ee4e338ea
jianjun <justmao945@gmail.com>
2015-03-26 14:36:46 +0800

    multiple To

--- smtp.rb/send.rb
+++ smtp.rb/send.rb
@@ -2,6 +2,7 @@ require 'net/smtp'
 
 from = "test@test.com"
 to = "justmao945@gmail.com"
+to1 = "jianjun@atrenta.com"
 cc = "327118385@qq.com"
 
 to, cc = cc, to
@@ -9,6 +10,7 @@ to, cc = cc, to
 msg = <<EOF
 From: demo <#{from}>
 To: Jianjun Mao <#{to}>
+To: Mao Jianjun <#{to1}>
 Cc: Jianjun Mao <#{cc}>
 Subject: SMTP.rb test
 Date: #{Time.now}
@@ -18,6 +20,6 @@ HaHa, this mail is sent by ruby.
 EOF
 
 Net::SMTP.start('localhost') do |smtp|
-    smtp.send_message msg, from, [to,cc]
+    smtp.send_message msg, from, [to,to1,cc]
 end
 
